32b320666cbec831cd47e9c3f6c6a57f434e9608
Michael Shuff <mjshuff23@gmail.com>
2020-08-06 17:47:17 -0400

    new game button half-working

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,12 @@
 window.addEventListener('DOMContentLoaded', event => {
     // Make a new array, board, that has 9 spots, and fill those spots with ''
-    const board = Array(9).fill('');
+    let board = Array(9).fill('');
     let count = 0;
     const player1 = "X";
     const player2 = "O";
     const gameStatus = document.getElementById("game-status");
     const game = document.getElementById('tic-tac-toe-board');
+    const newGameBtn = document.getElementById('new-game');
     let winner = false;
 
         game.addEventListener('click', event => {
@@ -39,33 +40,61 @@ window.addEventListener('DOMContentLoaded', event => {
         if (board[0] === board[1] && board[1] === board[2] && board[2] !== ""){
             gameStatus.innerHTML = `Winner: ${board[0]}`
             winner = true;
+            newGame();
         } else if (board[3] === board[4] && board[4] === board[5] &&  board[5] !== '') {
             gameStatus.innerHTML = `Winner: ${board[3]}`
             winner = true;
+            newGame();
         } else if (board[6] === board[7] && board[7] === board[8] && board[8] !== '') {
             gameStatus.innerHTML = `Winner: ${board[6]}`
             winner = true;
+            newGame();
         } else if (board[0] === board[3] && board[3] === board[6] && board[6] !== '') {
             gameStatus.innerHTML = `Winner: ${board[0]}`
             winner = true;
+            newGame();
         } else if (board[1] === board[4] && board[4] === board[7] && board[7] !== '') {
             gameStatus.innerHTML = `Winner: ${board[1]}`
             winner = true;
+            newGame();
         } else if (board[2] === board[5] && board[5] === board[8] && board[8] !== '') {
             gameStatus.innerHTML = `Winner: ${board[2]}`
             winner = true;
+            newGame();
         } else if (board[2] === board[4] && board[4] === board[6] && board[6] !== '') {
             gameStatus.innerHTML = `Winner: ${board[2]}`
             winner = true;
+            newGame();
         } else if (board[0] === board[4] && board[4] === board[8] && board[8] !== '') {
             gameStatus.innerHTML = `Winner: ${board[0]}`
             winner = true;
+            newGame();
         }
         if (count === 9) {
-            gameStatus.innerHTML = "Tie..."
+            gameStatus.innerHTML = "Winner: None";
+            winner = true;
+            newGame();
             return;
         }
     }
 
+    newGame = () => {
+        newGameBtn.addEventListener('click', event => {
+            // Clear Header, winner status, and board
+            if (winner === false) {
+                return;
+            }
+            gameStatus.innerHTML = '';
+            winner = false;
+            clearBoard();
+            board = Array(9).fill('');
+        });
+    };
 
+    clearBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            const board = document.getElementById(`square-${i}`);
+            board.innerHTML = '';
+        }
+    }
 });
