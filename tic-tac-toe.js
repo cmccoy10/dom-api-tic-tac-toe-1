282b2f77163f0b511cea875f8bb19d110fe8b4a4
Michael Shuff <mjshuff23@gmail.com>
2020-08-06 15:50:59 -0400

    got image showing

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,19 @@
 window.addEventListener('DOMContentLoaded', event => {
     const currentPlayerSymbol = 'x'
+    // Make a new array, squareValues, that has 9 spots, and fill those spots with ''
     const squareValues = Array(9).fill('');
 
     document.getElementById('tic-tac-toe-board').addEventListener('click', event => {
+        console.log(event.target.id);
         if (event.target.id.includes('square')) {
-            console.log(event.target.id);
+            const index = event.target.id[event.target.id.length - 1];
+            if (squareValues[index] === '') {
+                console.log('success');
+                squareValues[index] = 'x';
+                event.target.innerHTML = '<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg">';
+            }
         }
-        console.log('hi');
+
 
 
     });
